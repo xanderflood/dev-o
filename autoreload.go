5508ed4a3a9cde83304029eab9016404a5f17940
xanderflood <xander.t.flood@gmail.com>
2018-07-20 20:47:53 -0400

    rewrote core functions

---
+++ autoreload.go
@@ -0,0 +1,68 @@
+package devo
+
+import (
+	"sync"
+	"time"
+)
+
+//Autoreload asynchronously replace this process with updated versions
+//this process is compiled from the go file
+func Autoreload(options ...Option) (sync.Locker, error) {
+	c, err := configure(options...)
+	if err != nil {
+		return nil, err
+	}
+
+	binner := NewBinner(c)
+	watcher := NewGoFileWatcher(c.subjects)
+	go AutoreloadDaemon(
+		binner,
+		watcher,
+		c.waitMS,
+	)
+
+	// start //
+	return binner.lock, nil
+}
+
+//TODO much of this is a littl inconvenient to test, but
+//AutoreloadDaemon can and should be tested be pretty thoroughly
+
+//AutoreloadDaemon AutoreloadDaemon
+func AutoreloadDaemon(
+	binner Binner,
+	watcher Watcher,
+	waitMS uint64,
+) {
+	var updated, built, stateChanged bool
+	for {
+		if stateChanged {
+			//wait for the interval
+			<-time.NewTimer(time.Duration(waitMS) * time.Millisecond).C
+		} else {
+			stateChanged = true
+		}
+
+		if built {
+			binner.Exec()
+
+			//it didn't exec, try rebuilding
+			//send to the error log
+			built = false
+		} else if updated {
+			err := binner.Build()
+			if err != nil {
+				//TODO log
+				continue
+			}
+
+			built = true
+			stateChanged = false
+		} else {
+			if watcher.WasUpdated() {
+				updated = true
+				stateChanged = true
+			}
+		}
+	}
+}
